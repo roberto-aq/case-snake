ceac340ad03eab42b706ad314cb4f342500853e8
roberto-aq <baxro2000@gmail.com>
2024-07-10 17:07:37 -0500

    Fixed Navbar - Link dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = async () => {
 
 	const user = await getUser();
 
-	const isAdmin = user?.email === process.env.ADMIN_EMAIl;
+	const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
 	return (
 		<nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
